ced09b5724eaa92fd64e93a014c21ef9e2fd9901
olovm <olov@mckie.se>
2019-03-20 09:30:45 +0000

    splitted out cora-gatekeeperserver

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,5 @@
 module se.uu.ub.cora.gatekeeper {
-	requires java.ws.rs;
-	requires json;
-	requires javax.servlet.api;
-	requires bookkeeper;
-	requires java.activation;
-	// exports se.uu.ub.cora.gatekeeper;
+	requires transitive bookkeeper;
 
-	uses se.uu.ub.cora.gatekeeper.user.UserPickerProvider;
-	uses se.uu.ub.cora.gatekeeper.user.UserStorage;
+	exports se.uu.ub.cora.gatekeeper.user;
 }
--- src/main/java/se/uu/ub/cora/gatekeeper/user/UserPickerProvider.java
+++ src/main/java/se/uu/ub/cora/gatekeeper/user/UserPickerProvider.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2016, 2017 Uppsala University Library
+ * Copyright 2019 Olov McKie
  *
  * This file is part of Cora.
  *
